3ec26a9ae95d3100c7e0b9dd6c453f4ade26673e
rubimeryka <erykarubim13@gmail.com>
2022-07-05 11:15:30 -0300

    requisito 4

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -32,6 +33,9 @@ class Header extends React.Component {
                { userName }
              </div>
            )}
+         <Link data-testid="link-to-search" to="/search">Pesquisar</Link>
+         <Link data-testid="link-to-favorites" to="/favorites">Músicas Favoritas</Link>
+         <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
        </header>
      );
    }
